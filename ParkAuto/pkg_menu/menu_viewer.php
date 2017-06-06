25a198f2971fa2c0529d782430a18be52f94a77a
oiraMm <mariomoulin42@gmail.com>
2017-06-06 17:12:13 +0200

    ajout du menu admin

--- ParkAuto/pkg_menu/menu_viewer.php
+++ ParkAuto/pkg_menu/menu_viewer.php
@@ -59,34 +59,40 @@ class menu_viewer
     }
     public function templateMenuAdmin()
     {
+
         $case1 =new htmlForm('index.php', 'POST');
-        $case1->addBtSubmit('Accueil');
+        $case1->addBtSubmit('Accueil',"btn .btn-primary");
+        $collection[]=$case1;
         $case2 =new htmlForm('index.php', 'POST');
-        $case2->addBtSubmit('Etat du parc');
+        $case2->addBtSubmit('Etat du parc',"btn .btn-primary");
+        $collection[]=$case2;
         $case3 =new htmlForm('index.php', 'POST');
-        $case3->addBtSubmit('Mes reservations');
+        $case3->addBtSubmit('Mes reservations',"btn .btn-primary");
+        $collection[]=$case3;
         $case4 =new htmlForm('index.php', 'POST');
-        $case4->addBtSubmit('Validation');
+        $case4->addBtSubmit('Validation',"btn .btn-primary");
+        $collection[]=$case4;
         $case5 =new htmlForm('index.php', 'POST');
-        $case5->addBtSubmit('Signalement');
+        $case5->addBtSubmit('Signalement',"btn .btn-primary");
+        $collection[]=$case5;
         $case6 =new htmlForm('index.php', 'POST');
-        $case6->addBtSubmit('Profil');
+        $case6->addBtSubmit('Profil',"btn .btn-primary");
+        $collection[]=$case6;
         $case7 =new htmlForm('index.php', 'POST');
-        $case7->addBtSubmit('Administration');
+        $case7->addBtSubmit('Administration',"btn .btn-primary");
+        $collection[]=$case7;
         $case8 =new htmlForm('index.php', 'POST');
-        $case8->addBtSubmit('Deconnexion');
+        $case8->addBtSubmit('Deconnexion',"btn .btn-primary");
+        $collection[]=$case8;
         //création d'un tableau html pour le rendu
-        $table = new STable();
-        $table->tr()
-            ->td($case1->render())
-            ->td($case2->render())
-            ->td($case3->render())
-            ->td($case4->render())
-            ->td($case5->render())
-            ->td($case6->render())
-            ->td($case7->render())
-            ->td($case8->render());
-        //affichage du tableau
-        echo $table->getTable();
+        $menu="";
+        foreach ($collection as $item){
+            $menu.="<li class=\"nav-item\">".$item->render()."</li>";
+        }
+
+        $page=str_replace("%menu%",file_get_contents("pkg_graphique/navbar.html"),$menu);
+
+        echo $page;
+
     }
 }
