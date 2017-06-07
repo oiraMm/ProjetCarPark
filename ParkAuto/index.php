1bd8d9aeb56b571d7e4f21f343cab5493ceeb86b
Yael <berne.yael@yahoo.fr>
2017-06-07 09:55:48 +0200

    ajout du footer

--- ParkAuto/index.php
+++ ParkAuto/index.php
@@ -11,52 +11,38 @@ if (isset($_POST['Submit']) && isset($_SESSION['current_user']))
     $str_page_request = $_POST['Submit'];
     if ($str_page_request == 'Accueil')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         $obj_news_controller = new news_controller('actionAfficheAllNews');
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Etat du parc')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Etat du parc est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Mes reservations')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Mes reservations est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Signalement')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Signalement est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Profil')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Profil est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Administration')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Administration est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Validation')
     {
-        echo '<p>HEADER</p>';
         $obj_menu = new menu_controller();
         echo 'Validation est un écran à venir';
-        echo '<p>FOOTER</p>';
     }
     elseif ($str_page_request == 'Deconnexion')
     {
@@ -66,10 +52,8 @@ if (isset($_POST['Submit']) && isset($_SESSION['current_user']))
 }
 else if (isset($_SESSION['current_user']))
 {
-    echo '<p>HEADER</p>';
     $obj_menu = new menu_controller();
     $obj_news_controller = new news_controller('actionAfficheAllNews');
-    echo '<p>FOOTER</p>';
 }
 else
 {
@@ -82,3 +66,6 @@ else
         $obj_utilisateur_controller = new utilisateur_controller('actionAfficheConnexion');
     }
 }
+
+
+include_once 'pkg_graphique/footer.html';
